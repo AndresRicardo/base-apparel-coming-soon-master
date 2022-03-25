8a34632b48a9352342b5364a64938d4ef56b0bb8
Richi <ricaldo2431@gmail.com>
2022-03-24 23:36:25 -0500

    form validation ready

---
+++ js/app.js
@@ -0,0 +1,22 @@
+const emailInput = document.getElementById("emailInput");
+const validateMessage = document.getElementById("validateMessage");
+const submit = document.getElementById("submit");
+
+const regExpEmail =
+    /^[a-z0-9]+(\.[_a-z0-9]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,15})$/;
+
+submit.addEventListener("click", (e) => {
+    e.preventDefault(); // al tener esta opcion, el navegador no ejecuta las validaciones establecidas en el html
+    // e.stopPropagation();
+
+    if (!regExpEmail.test(emailInput.value)) {
+        validateMessage.textContent = "Please provide a valid email";
+        validateMessage.style.color = "hsl(0, 93%, 68%)";
+        validateMessage.style.display = "block";
+        return;
+    }
+
+    validateMessage.textContent = "Email sent";
+    validateMessage.style.color = "#20c997";
+    validateMessage.style.display = "block";
+});
